daff484fd5ced3e119406c3959d1f9783fe5ba1f
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    onclite: Enforce vendor tree presence
    A compiled build has no chance of booting without the prebuilt
    binaries, so fail the build if those are not present.
    
    Signed-off-by: Hadad <hadad@linuxmail.org>
    Change-Id: I8124a5f92a75490ce48422b9d760764733efe37e
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -201,4 +201,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
--include vendor/xiaomi/onclite/BoardConfigVendor.mk
+include vendor/xiaomi/onclite/BoardConfigVendor.mk
