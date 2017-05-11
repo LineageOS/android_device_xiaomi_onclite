59e38d570d12c5c18ad4506984f7228992784ec7
Umesh Vats <uvats@codeaurora.org>
2017-05-11 12:03:55 -0700

    onclite: Bluetooth: Increase number of SEPs to support SHO/Mcast
    Icreased number of SEPs to support SHO/Mcast
    
    Change-Id: I3cfb4e8d4d6969e442406bd095a865d49947151d

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -27,4 +27,7 @@
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BTM_WBS_INCLUDED TRUE
 #define BTIF_HF_WBS_PREFERRED TRUE
+
+/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
+#define AVDT_NUM_SEPS 12
 #endif
