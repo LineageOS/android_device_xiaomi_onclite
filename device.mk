1b3d882864c898cd3c068405ed92ae9a94951ce7
Alexander Martinz <alex@amartinz.at>
2017-11-06 13:41:15 +0100

    onclite: bluetooth: Build libbthost_if
    Change-Id: I55a5e5260301cfd658ff8bcd0015f127eefeaa39
    Signed-off-by: Alexander Martinz <alex@amartinz.at>
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- device.mk
+++ device.mk
@@ -110,6 +110,10 @@ PRODUCT_COPY_FILES += \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbthost_if
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
