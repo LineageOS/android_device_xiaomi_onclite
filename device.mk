a32004383a0d7dec1faad8bebc12a1ee1f08b791
blong <blong@codeaurora.org>
2018-03-28 16:29:43 +0800

    onclite: add new irqbalance conf file for sdm632
    - cpu0~3 is the primary cpu for sdm632,need put the irqbalance
      on it, add the new conf file to realise it
    
    CRs-Fixed: 2196375
    
    Change-Id: Ifadd040a8f93ec32853008290794ae19d12812c4

--- device.mk
+++ device.mk
@@ -226,7 +226,7 @@ PRODUCT_PACKAGES += \
 
 # IRQ
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+    $(LOCAL_PATH)/configs/msm_irqbalance_sdm632.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_sdm632.conf
 
 # IRSC
 PRODUCT_COPY_FILES += \
