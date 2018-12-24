8821f818dc953bc16f64044933e94ce727f98659
dianlujitao <dianlujitao@lineageos.org>
2018-12-24 12:04:48 +0800

    onclite: Support Alipay and WeChat fingerprint payment
     * IFAA manager is based on OnePlusOSS, but adapted for Xiaomi's mlipay
       interface.
    
    Change-Id: Ied17d6456561bb399e4dd4a868d57a48d2d51db2

--- device.mk
+++ device.mk
@@ -223,6 +223,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
+# IFAA manager
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
