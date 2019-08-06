fe0b356ad40901c11c961e14e4c87e7d6c60cd49
Prakash Pabba <ppabba@codeaurora.org>
2019-08-06 15:27:55 +0530

    onclite: Enable ipsec_tunnel feature
    Fixed : CTS issue due to ipsec_tunnel feature.
    
    CTS issues are showing failure on devices with first_api_level set to
    29 . With this api level certain test cases which weren't executing
    till now begin executing. Ex: android.net.cts.IpSecManagerTunnelTest.
    
    These tests mandatorily requrire ipsec_tunnel feature to be enabled.
    
    Change-Id: If52b652ac3cc6488fefcf851527f224efa03e7eb

--- device.mk
+++ device.mk
@@ -42,6 +42,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
