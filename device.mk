6bc75cfb23cf8a1dbaed794fe075b37ce4da7ce9
Dhina17 <dhinalogu@gmail.com>
2019-09-19 15:40:08 +0000

    onclite: Import sensor configs from stock
    -From onc-user-9-PKQ1.181021.001-V10.3.3.0.PFFINXM-release-keys
    
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- device.mk
+++ device.mk
@@ -320,6 +320,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
