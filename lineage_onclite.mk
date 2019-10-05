95d88fe8f291c2481c20f2fda3bd1ea4eed1dd03
Dhina17 <dhinalogu@gmail.com>
2019-10-05 12:34:37 +0000

    onclite: Bringup LineageOS
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

---
+++ lineage_onclite.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from onclite device
+$(call inherit-product, device/xiaomi/onclite/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_onclite
+PRODUCT_DEVICE := onclite
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 7
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "xiaomi/onc/onc:9/PKQ1.181021.001/V10.3.3.0.PFLINXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V10.3.3.0.PFLINXM release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
