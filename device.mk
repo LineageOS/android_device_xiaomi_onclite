79612974a2ae6258a67424d39f81090049a1fbf3
Dhina17 <dhinalogu@gmail.com>
2019-10-18 03:05:35 +0000

    onclite: Build health HIDL service and impl
    ..from source
    
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- device.mk
+++ device.mk
@@ -166,6 +166,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-service \
+    android.hardware.health@2.0-impl
+
 # Healthd
 PRODUCT_PACKAGES += \
     chargeonlymode
