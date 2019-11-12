aac030925d6194f23cf00fd4fdadd8267d658318
Dhina17 <dhinalogu@gmail.com>
2019-11-12 19:13:36 +0000

    onclite: Import init.class_main.sh from stock
    - start the RIL services
    
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -71,6 +71,14 @@ include $(BUILD_PREBUILT)
 # Configuration scripts
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.class_main.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.class_main.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
