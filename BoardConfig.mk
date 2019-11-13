186989ad2682f1821084ce5812c815e67a502ad9
xingrz <chenxingyu92@gmail.com>
2019-11-13 08:01:53 +0000

    onclite: releasetools: Add dtbo image to the output zip if present
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,6 +200,9 @@ TARGET_RIL_VARIANT := caf
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
