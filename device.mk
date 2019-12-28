b809639c98b94dbff7ffe015d2cdf45687d448a8
Dhina17 <dhinalogu@gmail.com>
2019-12-28 10:29:06 +0100

    onclite: Import Power HAL from LA.UM.8.6.r1-02900-89xx.0
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- device.mk
+++ device.mk
@@ -273,7 +273,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.0-service \
+    android.hardware.power@1.0-impl \
+    power.qcom
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
