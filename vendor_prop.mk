87fb1bdffcdd3f86d0f697d0166d6d1978be9a1b
Dhina17 <dhinalogu@gmail.com>
2019-12-31 22:20:13 +0100

    onclite: Rearrange the props
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,3 +1,4 @@
+# Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
     audio.deep_buffer.media=true \
@@ -42,9 +43,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.voice.path.for.pcm.voip=true \
     vendor.voice.playback.conc.disabled=true \
     vendor.voice.record.conc.disabled=false \
-    vendor.voice.voip.conc.disabled=true \
-    persist.cne.feature=1 \
-    persist.vendor.cne.feature=1 \
+    vendor.voice.voip.conc.disabled=true
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.qcom.bluetooth.soc=smd
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.awb.sync=2 \
     persist.vendor.camera.display.lmax=1280x720 \
     persist.vendor.camera.display.umax=1920x1080 \
@@ -53,21 +59,29 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.qualcomm.qti.qmmi,com.longcheertel.cit \
     vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
-    vendor.camera.lowpower.record.enable=1 \
-    ro.cutoff_voltage_mv=3400 \
-    persist.dpm.feature=1 \
-    drm.service.enabled=true \
+    vendor.camera.lowpower.record.enable=1
+
+# CnE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.cne.feature=1 \
+    persist.vendor.cne.feature=1
+
+# Coresight
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.coresight.config=stm-events
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapmaxfree=8m \
     dalvik.vm.heapminfree=4m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heapstartsize=16m \
     dalvik.vm.heaptargetutilization=0.75 \
-    ro.dalvik.vm.native.bridge=0 \
-    ro.fm.transmitter=false \
-    vendor.hw.fm.init=0 \
-    ro.frp.pst=/dev/block/bootdevice/by-name/config \
-    persist.fuse_sdcard=true \
+    ro.dalvik.vm.native.bridge=0
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=0 \
     debug.gralloc.enable_fb_ubwc=1 \
     debug.sf.enable_hwc_vds=1 \
@@ -77,23 +91,50 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
     ro.opengles.version=196610 \
+    ro.qualcomm.cabl=0 \
     ro.vendor.display.cabl=0 \
     ro.vendor.display.sensortype=2 \
     ro.vendor.display.svi=1 \
+    sdm.debug.disable_skip_validate=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=0 \
     vendor.display.svi.config=1 \
     vendor.display.svi.config_path=/system/etc/display/SVIConfig.xml \
-    vendor.gralloc.enable_fb_ubwc=1 \
+    vendor.gralloc.enable_fb_ubwc=1
+
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dpm.feature=1
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.qfp=false
+
+# FM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fm.transmitter=false \
+    vendor.hw.fm.init=0
+
+# FRP
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/config
+
+# Location
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.location.osnlp.package=com.google.android.gms \
-    ro.location.osnlp.region.package= \
+    ro.location.osnlp.region.package=
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
     media.aac_51_output_enabled=true \
     media.msm8956hw=0 \
     mm.enable.smoothstreaming=true \
     mmp.enable.3g2=true \
     persist.mm.sta.enable=0 \
-    ro.emmc_size=16GB \
-    ro.qualcomm.cabl=0 \
     vendor.mm.enable.qcom_parser=1048575 \
     vendor.vidc.dec.downscalar_height=1088 \
     vendor.vidc.dec.downscalar_width=1920 \
@@ -104,34 +145,23 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.enable.sglscale=1 \
     debug.mdpcomp.logs=0 \
     keyguard.no_require_sim=true \
-    persist.debug.coresight.config=stm-events \
     persist.hwc.enable_vds=1 \
     persist.hwc.mdpcomp.enable=true \
-    persist.qfp=false \
-    persist.sys.df.extcolor.proc=0 \
-    persist.sys.job_delay=true \
-    persist.sys.sar.dsi=7 \
-    persist.vendor.qcomsysd.enabled=1 \
-    persist.vendor.qg.log_level=1 \
-    ro.colorpick_adjust=true \
-    ro.hardware.keystore_desede=true \
-    ro.logd.size=1M \
-    ro.vendor.df.effect.conflict=1 \
-    sys.boe_hx8394f_length=35 \
-    sys.boe_hx8394f_offset=11 \
-    sys.paper_mode_max_level=32 \
-    sys.truly_otm1901a_length=42 \
-    sys.truly_otm1901a_offset=1 \
-    vendor.debug.enable.gamed=0 \
-    ro.hardware.nfc_nci=nqx.default \
-    persist.backup.ntpServer=0.pool.ntp.org \
+    vendor.video.disable.ubwc=1
+
+# Netmgrd
+PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=true \
     persist.vendor.data.mode=concurrent \
-    ro.vendor.use_data_netmgrd=true \
-    ro.memperf.enable=false \
-    ro.memperf.lib=libmemperf.so \
+    ro.vendor.use_data_netmgrd=true
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
-    vendor.perflocks_customized_for_apps=1 \
+    vendor.perflocks_customized_for_apps=1
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.qti.telephony.vt_cam_interface=1 \
     DEVICE_PROVISIONED=1 \
     persist.radio.multisim.config=dsds \
@@ -152,13 +182,47 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=true \
     ro.telephony.default_network=22,20 \
     telephony.lteOnCdmaDevice=1 \
-    vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
+
+# Storage
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.fuse_sdcard=true
+
+# Thermal
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.thermal.data.path=/data/vendor/thermal/
+
+# Time service
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true
+
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.diag_mdlog_enable=false
+
+# Wifi-display
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.wfd.enable=1
+
+# Misc
+PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.ssr.restart_level=ALL_ENABLE \
     sys.vendor.shutdown.waittime=500 \
-    sdm.debug.disable_skip_validate=1 \
-    sys.thermal.data.path=/data/vendor/thermal/ \
-    persist.timed.enable=true \
-    vendor.video.disable.ubwc=1 \
-    persist.sys.usb.diag_mdlog_enable=false \
-    persist.debug.wfd.enable=1 \
-    vendor.qcom.bluetooth.soc=smd \
+    persist.sys.df.extcolor.proc=0 \
+    persist.sys.job_delay=true \
+    persist.sys.sar.dsi=7 \
+    persist.vendor.qcomsysd.enabled=1 \
+    persist.vendor.qg.log_level=1 \
+    ro.colorpick_adjust=true \
+    ro.hardware.keystore_desede=true \
+    ro.logd.size=1M \
+    ro.vendor.df.effect.conflict=1 \
+    sys.boe_hx8394f_length=35 \
+    sys.boe_hx8394f_offset=11 \
+    sys.paper_mode_max_level=32 \
+    sys.truly_otm1901a_length=42 \
+    sys.truly_otm1901a_offset=1 \
+    ro.hardware.nfc_nci=nqx.default \
+    persist.backup.ntpServer=0.pool.ntp.org \
+    ro.emmc_size=16GB \
+    ro.cutoff_voltage_mv=3400
