f7597444feb22c38f1f3bcace9b7d4ebc88b110a
Tanmay Garg <tanmayg@codeaurora.org>
2020-04-13 10:19:45 +0530

    Revert "wifi: Add move_wifi_data.sh"
    This partially reverts commit 00bcf7dbaab439a27cb4ddf443ac45d49036954a.
    
    CRs-Fixed: 2662406
    Change-Id: Icd9d198c69aaf4d689f08bf5700f5203a69d4939

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -110,10 +110,3 @@ LOCAL_SRC_FILES    := bin/move_time_data.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := move_wifi_data.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/move_wifi_data.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
-include $(BUILD_PREBUILT)
