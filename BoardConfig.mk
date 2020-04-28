f07204619dec9eee8cd38be1e2475856665d1331
Jyotiraditya <jyotiraditya@aospa.co>
2020-04-28 04:55:54 +0000

    onc: Show correct device and model name as per boot cert.
    Signed-off-by: Jyotiraditya <dreadnaught02@outlook.com>
    Change-Id: Ie2a83bf0e2168aaebe1427edb8bd8f8ee4490e56

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,6 +160,10 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_onc
+TARGET_RECOVERY_DEVICE_MODULES := libinit_onc
+
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
