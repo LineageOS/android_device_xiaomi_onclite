a0d6b5123a687cc99c79a4ead3ba11fffeea963c
Jyotiraditya <jyotiraditya@aospa.co>
2020-04-28 04:55:54 +0000

    onclite: init: Set correct device and model name as per boot cert.
    * Redmi 7 and Redmi Y3 are nearly same device except Front Cam and Gyroscope sensor.
    
    * Redmi Y3 has 32MP selfie cam that is an AUX cam which is a cursed part in custom roms.
      And Gyroscope sensor works well in Y3 since we are including permission config which won't affect Redmi 7 as well
    
    * So Let make Y3 users happy by setting device name and model in init.
    
    Signed-off-by: Jyotiraditya <jyotiraditya@aospa.co>
    Change-Id: Ie2a83bf0e2168aaebe1427edb8bd8f8ee4490e56
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,6 +159,10 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_onclite
+TARGET_RECOVERY_DEVICE_MODULES := libinit_onclite
+
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
