05aa814a666a9a22c4c84c24708b61d8ba957456
Nicholas Lim <nicknitewolf@lineageos.org>
2020-05-18 17:06:12 +0800

    onclite: Drop FDE support entirely
    Change-Id: Ifecd15ee6c86a4571e5a7cc961372348455c4d5c

--- device.mk
+++ device.mk
@@ -210,10 +210,6 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
