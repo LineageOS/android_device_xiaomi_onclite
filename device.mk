093adf15a76c06a572200573762491eb0465d76b
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2020-06-01 13:13:08 +0530

    onclite: Remove deprecated android.hardware.thermal@1.0 HAL
    Remove deprecated android.hardware.thermal@1.0 HAL from
    device manifest.
    
    Bug: 154012150
    Change-Id: I884e6b96cac4ebc3abcfaf309388d526a30dc5bd

--- device.mk
+++ device.mk
@@ -377,12 +377,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.msm8953
-
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
