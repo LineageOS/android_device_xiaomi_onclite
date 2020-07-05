3d3368409bd6e084d4f2fb025a20f78e9bbd6abf
Sai Kousik Swarna <sswarna@codeaurora.org>
2020-07-05 11:32:08 +0530

    onclite: Update secure_element interface version
    Update the secure_element version to enable new upgraded interface solution.
    
    Change-Id: Iaefba8ee8e459b3332a40ffc8f6c55d5e1e68df3

--- device.mk
+++ device.mk
@@ -356,7 +356,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.0.vendor \
+    android.hardware.secure_element@1.2.vendor \
     librmnetctl \
     libcnefeatureconfig \
     libxml2 \
