01f1e4b4b527f1393bd412200ae67183c72b3da2
Dhina17 <dhinalogu@gmail.com>
2020-08-07 13:38:28 +0530

    onclite: Inherit common 2GB dalvik heap config
    Change-Id: I97e5e60ae7e8dd03129247028cd02b9c56f9c824

--- device.mk
+++ device.mk
@@ -128,6 +128,13 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor \
     Snap
 
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapstartsize=16m
+
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
