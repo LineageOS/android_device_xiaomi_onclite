092e8a3abfcfb3525da72577bee0735fdd923e1f
Dhina17 <dhinalogu@gmail.com>
2020-08-23 02:11:16 +0530

    onclite: Set Boot image header version
    Change-Id: Ifebdf90c0ddf748229c4f9c8458ae7858379a79a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,7 +45,8 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78af000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+BOARD_BOOTIMG_HEADER_VERSION := 1
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/onclite
 TARGET_KERNEL_CONFIG := onclite-perf_defconfig
 BOARD_KERNEL_SEPARATED_DTBO := true
