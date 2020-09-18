23bb6ebe8867ccd033718c1394b97e53ca204153
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    onclite: Update qcom sepolicy include
    - CAF also moved away from Android.mk but choosed to name the
    makefile as "SEPolicy.mk" which we were naming as "sepolicy.mk"
    in LineageOS
    
    Ref:
    [0]: https://github.com/LineageOS/android_device_qcom_sepolicy/commit/1ca2d270146a2137876d52648a2ba21ac6fd50c1
    
    Change-Id: Ic5a24b8e469342529f7d8a8b0e1838bf9f141987

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,7 +199,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Treble
