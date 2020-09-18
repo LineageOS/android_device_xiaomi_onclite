8208e969a3bfde5176ca777c7f2273bbb22ed15c
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    onclite: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: Ifa9285fa31def64bd30c03f607d5674ea9fd79af

--- device.mk
+++ device.mk
@@ -200,7 +200,11 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0_system \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # HW crypto
 PRODUCT_PACKAGES += \
