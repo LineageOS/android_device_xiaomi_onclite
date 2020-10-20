4d14b640f962175e45c8f0462f8236625cb46b75
Quallenauge <Hamsi2k@freenet.de>
2020-10-20 23:19:01 +0200

    onclite: wifi: Flag device with broken interface mode implementation.
    Change-Id: I1ecbc67efc9780dd1684f1b403cb5ce2edaa9d5e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -224,6 +224,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+TARGET_HAS_BROKEN_WLAN_SET_INTERFACE := true
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/onclite/BoardConfigVendor.mk
