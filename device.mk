9b43062e78c73e7b1d11fb40d7276a0d8a2035a6
Edwin Wong <edwinwong@google.com>
2021-03-25 14:36:24 -0700

    onclite: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I406b7d3c2921d4698535421894c6eb3785eb1c0d

--- device.mk
+++ device.mk
@@ -168,7 +168,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # Ebtables
 PRODUCT_PACKAGES += \
