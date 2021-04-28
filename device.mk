8224115d188f93d06fd104b7d88c69440a084ac0
Dhina17 <dhinalogu@gmail.com>
2021-04-28 22:39:31 +0530

    [DNM] Bringup
    Change-Id: I935302d760f80fb926845e69a1514585267ae513

--- device.mk
+++ device.mk
@@ -160,8 +160,8 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.9
 
 # Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
+#PRODUCT_PACKAGES += \
+#    XiaomiDoze
 
 # DRM
 PRODUCT_PACKAGES += \
