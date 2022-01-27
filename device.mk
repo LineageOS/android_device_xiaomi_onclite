1fb904c09f35d9691d3b13ba8abc5a1a5ffcbbac
Alexander Koskovich <akoskovich@pm.me>
2022-01-27 14:23:56 -0700

    onclite: Drop support for Snapdragon Camera.
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- device.mk
+++ device.mk
@@ -126,8 +126,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libgui_vendor \
-    libstdc++.vendor \
-    Snap
+    libstdc++.vendor
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
