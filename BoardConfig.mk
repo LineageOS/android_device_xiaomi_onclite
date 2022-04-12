a399e6041964f84d3beafa0f542fa03a7bd297e0
Alexander Koskovich <akoskovich@pm.me>
2022-04-11 21:13:13 -0700

    onclite: Sort kernel cmdline and additional flags as a list
     * This allows better readability and eases tracking further changes.
    
    Change-Id: I3a23f7ccb238b5f0e18dfa18080f1ae8ff51109f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,7 +103,18 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_onclite
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78af000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7
+BOARD_KERNEL_CMDLINE := \
+    androidboot.bootdevice=7824900.sdhci \
+    androidboot.console=ttyMSM0 \
+    androidboot.hardware=qcom \
+    androidboot.usbconfigfs=true \
+    console=ttyMSM0,115200,n8 \
+    earlycon=msm_serial_dm,0x78af000 \
+    ehci-hcd.park=3 \
+    firmware_class.path=/vendor/firmware_mnt/image \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_BOOTIMG_HEADER_VERSION := 1
