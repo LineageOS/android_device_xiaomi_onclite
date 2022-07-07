c582d1cc9d28bce403cc2b734d18f461fa788d46
Dhina17 <dhinalogu@gmail.com>
2022-07-07 22:56:16 +0530

    onclite: Build vendor.qti.hardware.camera.device@1.0
    - Needed by IMS VT
    
    Fixes:
    E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "vendor.qti.hardware.camera.device@1.0.so" not found: needed by /system/product/lib64/lib-imsvideocodec.so in namespace classloader-namespace-shared
    
    Change-Id: Ib90ffdb3209ea1486ab8286902bd1884c6775af9

--- device.mk
+++ device.mk
@@ -125,7 +125,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libgui_vendor \
-    libstdc++.vendor
+    libstdc++.vendor \
+    vendor.qti.hardware.camera.device@1.0
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
