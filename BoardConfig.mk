bbdc19f363fff176c68489f9187ea73757967a44
Dhina17 <dhinalogu@gmail.com>
2022-10-25 11:26:24 +0530

    onclite: sepolicy: Move to sepolicy/vendor
    Change-Id: I7305d622b689d9cda652b36234b549bd717ded97

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -173,7 +173,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
 BOARD_VNDK_VERSION := current
