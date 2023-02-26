5e6a1bce8afb8d68f723b2cc0fccb4abf7a49d53
me-cafebabe <me.cafebabe@gmail.com>
2023-02-26 19:47:51 +0800

    onclite: parts: Override common parts
    * onclite doesn't support Dirac, have to keep separated from them
    
    Change-Id: Ib310fa01186bd64bb2e468b02a7579125e98e203

--- device.mk
+++ device.mk
@@ -35,6 +35,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
+# Device-specific Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts_onclite
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953
