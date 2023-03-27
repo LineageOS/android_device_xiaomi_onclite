ddfa1295b9f87e9790c3e93f7700a54cb04d7896
Gabriel Biren <gbiren@google.com>
2023-03-27 22:33:41 +0000

    onclite: Update makefile to use the AIDL Wifi Vendor HAL.
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- device.mk
+++ device.mk
@@ -386,7 +386,7 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     libcld80211 \
     libwpa_client \
     hostapd \
