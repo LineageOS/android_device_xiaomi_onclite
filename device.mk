9b39c65dbaaa7c5ef24132878b0761095ed28d1f
Hadad <hadad@linuxmail.org>
2024-09-12 13:51:25 +0000

    onclite: Show correct battery design capacity in settings.
    * After https://review.lineageos.org/c/LineageOS/android_packages_apps_Settings/+/387317
      Some devices, including onclite. Settings cannot read mAh capacity correctly.
      And only showing 4 mAh.
    
    * This value can be fixed through RRO overlay. So that Setting will show the mAh value accurately.
    
    Test: Compile, see Battery Design Capacity in
          Settings > Battery > Battery Information.
    
    Change-Id: Ib08911b0c186f806342d326e1d77698343a336ee

--- device.mk
+++ device.mk
@@ -30,6 +30,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    OncBatteryOverlay \
     OncWifiOverlay \
     WifiOverlay
 
