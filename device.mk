2c2a8a13dfcb5c38b98c26ebd8535d06c174f92c
Michael Bestas <mkbestas@lineageos.org>
2025-01-04 09:09:32 +0200

    onclite: Remove OMX audio codecs
     * OMX HW audio codecs won't work without QCOM A/V enhancements, which
       we don't have on Lineage.
     * OMX SW audio codecs no longer exist.
    
    Change-Id: I940be6716b603e848c44839bb87ad690eb55e86f

--- device.mk
+++ device.mk
@@ -233,11 +233,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
     libmm-omxcore \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw \
